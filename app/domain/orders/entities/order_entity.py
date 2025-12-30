d0cc50db5bbc864db6e0d8143f3bbd12a23bc649
AbelChocca <abelchocca1010@gmail.com>
2025-12-30 09:53:17 -0500

    added two classmethod, create_new and from_persistence on OrderEntity

--- app/domain/orders/entities/order_entity.py
+++ app/domain/orders/entities/order_entity.py
@@ -1,27 +1,71 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Optional, Union
 from uuid import uuid4
+from decimal import Decimal
 
 class OrderEntity:
     def __init__(
             self,
-            tracking_token: str,
+            *,
             quantity: int,
-            unit_price: float,
-            total_price: float,
+            unit_price: Decimal,
+            total_price: Decimal,
             created_at: datetime,
+            tracking_token: str,
             status: str = "pending", 
             id: Optional[int] = None,
             product_id: Optional[int] = None
             ):
-        self.tracking_token: str = tracking_token
         self.quantity: int = quantity
-        self.unit_price: float = unit_price
-        self.total_price: float = total_price
-        self.created: datetime = created_at
+        self.unit_price: Decimal = unit_price
+        self.total_price: Decimal = total_price
+        self.created_at: datetime = created_at
         self.status: str = status
+        self.tracking_token: str = tracking_token
         self.id: Union[int, None] = id
         self.product_id: Union[int, None] = product_id
+    
+    @classmethod
+    def create_new(cls, quantity: int, unit_price: int, user_id: Optional[int] = None):
+        if quantity <= 0:
+            raise ValueError("Quantity must be greater than zero")
 
-    def generate_tracking_token(self, user_id: Optional[int] = None) -> str:
-        return f"order-{uuid4()}-{user_id if user_id is not None else 0}"
+        if unit_price <= 0:
+            raise ValueError("Unit price must be greater than zero")
+        
+        tracking_token: str = f"order-{uuid4()}-{user_id or 0}"
+
+        total: Decimal = quantity * unit_price
+
+        return cls(
+            quantity=quantity,
+            unit_price=unit_price,
+            total_price=total,
+            created_at=datetime.now(timezone.utc),
+            status="pending",
+            tracking_token=tracking_token
+        )
+    
+    @classmethod
+    def from_persistence(
+        cls,
+        *,
+        id: int,
+        quantity: int,
+        unit_price: Decimal,
+        total_price: Decimal,
+        created_at: datetime,
+        status: str,
+        tracking_token: str,
+        product_id: Optional[int] = None,
+    ):
+        return cls(
+            id=id,
+            quantity=quantity,
+            unit_price=unit_price,
+            total_price=total_price,
+            created_at=created_at,
+            status=status,
+            tracking_token=tracking_token,
+            product_id=product_id,
+        )
