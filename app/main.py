61f63ae86a34dfd4bf1e6b897c13a84304e34000
AbelChocca <abelchocca1010@gmail.com>
2026-01-01 09:55:38 -0500

    main: add graphql_router on FastApi router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import contextmanager
+from contextlib import asynccontextmanager
 
 from app.core.settings.pydantic_setitngs import settings
 from app.core.clients.cloudinary import init_cloudinary_client
 
-@contextmanager
+from app.api.graphql.router import graphql_router
+
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     init_cloudinary_client()
     yield 
@@ -23,3 +25,5 @@ app.add_middleware(
     allow_methods = ['*'],
     allow_headers = ['*']
 )
+
+app.include_router(graphql_router, prefix="/graphql")
