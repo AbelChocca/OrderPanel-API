c123be1a068e00eb67ae48e38f42b7b6448fc62d
AbelChocca <abelchocca1010@gmail.com>
2026-01-02 21:52:19 -0500

    main: add kafka producer on lifespan manager of fastapi app

--- app/main.py
+++ app/main.py
@@ -5,12 +5,17 @@ from contextlib import asynccontextmanager
 from app.core.settings.pydantic_setitngs import settings
 from app.core.clients.cloudinary import init_cloudinary_client
 
+from app.infra.messaging.kafka.clients.producer import kafka_producer
+
 from app.api.graphql.router import graphql_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_cloudinary_client()
+    await kafka_producer.start()
     yield 
+
+    await kafka_producer.stop()
 
 app = FastAPI(
     title="Real Time Orders Panel API",
