2249a1408d284a237f5a3f251c14ee7fb354dcd3
AbelChocca <abelchocca1010@gmail.com>
2026-01-02 21:46:22 -0500

    infra(kafka): initialize kafka consumer client

---
+++ app/infra/messaging/kafka/clients/consumer.py
@@ -0,0 +1,29 @@
+from aiokafka import AIOKafkaConsumer
+from typing import Iterable
+
+from app.infra.messaging.kafka.clients.event_router import KafkaEventRouter
+
+class KafkaConsumer:
+    def __init__(
+            self,
+            topics: Iterable[str],
+            bootstrap_servers: str,
+            handler: KafkaEventRouter,
+            group_id: str
+            ):
+        self.consumer: AIOKafkaConsumer = AIOKafkaConsumer(
+            *topics,
+            bootstrap_servers=bootstrap_servers,
+            group_id=group_id,
+            enable_auto_commit=False
+        )
+        self.handler: KafkaEventRouter = handler
+
+    async def start(self) -> None:
+        await self.consumer.start()
+        try:
+            async for msg in self.consumer:
+                await self.handler(msg)
+                await self.consumer.commit()
+        finally:
+            await self.consumer.stop()
