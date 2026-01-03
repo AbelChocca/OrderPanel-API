490e1226b83143b34dcbaf34521ef8df97d5c15e
AbelChocca <abelchocca1010@gmail.com>
2026-01-02 21:47:08 -0500

    infra(kafka): implement kafka event router client to handle many handlers

---
+++ app/infra/messaging/kafka/clients/event_router.py
@@ -0,0 +1,18 @@
+from typing import Dict, Callable, Optional
+from aiokafka import ConsumerRecord
+
+class KafkaEventRouter:
+    def __init__(
+            self,
+            handlers: Dict[str, Callable]
+            ):
+        self.handlers: Dict[str, Callable] = handlers
+
+    async def __call__(self, msg: ConsumerRecord) -> None:
+        topic: str = msg.topic
+
+        handler: Optional[Callable] = self.handlers.get(topic)
+        if not handler:
+            raise ValueError(f"There's not any handler about {topic}")
+
+        await handler(msg)
