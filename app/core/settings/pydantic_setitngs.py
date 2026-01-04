05919739bb8b6bb0be0ff1826b04a8856b1f89d8
AbelChocca <abelchocca1010@gmail.com>
2026-01-03 20:55:29 -0500

    core(settings): added new variables for db, kafka and email service to settings

--- app/core/settings/pydantic_setitngs.py
+++ app/core/settings/pydantic_setitngs.py
@@ -3,11 +3,27 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import List
 
 class Settings(BaseSettings):
+    # DB
+    DATABASE_URL: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    POSTGRES_DB: str
+
+    # sendgrid
+    SENDGRID_API_KEY: str
+    SENGRID_API_KEY_ID: str
+    SENGRID_API_KEY_NAME: str
+
+    FROM_EMAIL: str
+
     CLOUDINARY_CLOUD_NAME: str
     CLOUDINARY_API_KEY: str
     CLOUDINARY_API_SECRET: str
 
     ALLOW_ORIGINS: List[str]
+
+    KAFKA_BOOTSTRAP_SERVERS: str
+    KAFKA_CLIENT_ID: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
