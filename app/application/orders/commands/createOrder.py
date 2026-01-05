aa5302aa0d81afe1696de7f17d379a3a255ca162
AbelChocca <abelchocca1010@gmail.com>
2026-01-04 22:40:35 -0500

    app(orders)(commads): publish createOrder event in CreateOrderCase

--- app/application/orders/commands/createOrder.py
+++ app/application/orders/commands/createOrder.py
@@ -4,7 +4,7 @@ from app.infra.messaging.event_bus import EventBus
 from app.domain.orders.entities.order_entity import OrderEntity
 from app.domain.orders.entities.order_shipping_entity import OrderShippingEntity
 
-from app.application.orders.command_inputs import CreateOrderCommad
+from app.application.orders.inputs.command_inputs import CreateOrderCommad
 
 class CreateOrderCase:
     def __init__(
@@ -37,6 +37,25 @@ class CreateOrderCase:
         order = await self.order_repo.save(order)
 
         await self.event_bus.publish(
-            event_name="orders.created"
+            event_name="orders.created",
+            payload={
+                "event_name": "OrderCreated",
+                "version": "v1",
+                "payload": {
+                    "quantity": order.quantity,
+                    "total_price": order.total_price,
+                    "created_at": order.created_at,
+                    "tracking_token": order.tracking_token,
+                    "status": order.status,
+                    "full_name": order_shipping.full_name,
+                    "email": order_shipping.email,
+                    "address_line": order_shipping.address_line,
+                    "city": order_shipping.city,
+                    "region": order_shipping.region,
+                    "country": order_shipping.country,
+                    "postal_code": order_shipping.postal_code,
+                    "delivery_notes": order_shipping.delivery_notes
+                }
+            }
         )
         
