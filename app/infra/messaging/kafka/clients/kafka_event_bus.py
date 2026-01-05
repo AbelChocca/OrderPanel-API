2ed29797e0912b15773fd276a66daa6da4903915
AbelChocca <abelchocca1010@gmail.com>
2026-01-04 22:37:37 -0500

    infra(messaging)(kafka)(clients): added singleton of kafka event bus

--- app/infra/messaging/kafka/clients/kafka_event_bus.py
+++ app/infra/messaging/kafka/clients/kafka_event_bus.py
@@ -1,5 +1,6 @@
 from app.infra.messaging.event_bus import EventBus
-from app.infra.messaging.kafka.producer import KafkaProducer
+from app.infra.messaging.kafka.clients.producer import KafkaProducer, kafka_producer
+from functools import lru_cache
 
 from typing import Dict, Any
 
@@ -7,6 +8,9 @@ class KafkaEventBus(EventBus):
     def __init__(self, producer: KafkaProducer):
         self._producer: KafkaProducer = producer
 
-
     async def publish(self, event_name: str, payload: Dict[str, Any]):
         return await self._producer.publish(topic=event_name, value=payload)
+    
+@lru_cache
+def get_kafka_event_bus() -> KafkaEventBus:
+    return KafkaEventBus(kafka_producer)
