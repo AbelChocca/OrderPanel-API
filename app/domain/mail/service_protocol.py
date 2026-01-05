0d1f9e2f156c372ce07835fdc71b7d14e4a31de4
AbelChocca <abelchocca1010@gmail.com>
2026-01-04 22:38:59 -0500

    solve some type hints

--- app/domain/mail/service_protocol.py
+++ app/domain/mail/service_protocol.py
@@ -1,6 +1,4 @@
-from typing import Protocol, Union, List, Optional
-from datetime import datetime
-from decimal import Decimal
+from typing import Protocol, Union, List
 
 class MailService(Protocol):
     def send_email(
@@ -10,22 +8,4 @@ class MailService(Protocol):
         html_content: str,
         from_email: str
     ) -> None:
-        ...
-
-    def generate_order_html_content(
-        self, 
-        quantity: int,
-        total_price: Decimal,
-        created_at: datetime,
-        tracking_token: str,
-        status: str,
-        full_name: str,
-        email: str,
-        address_line: str,
-        city: str,
-        region: str,
-        country: str,
-        postal_code: Optional[int] = None,
-        delivery_notes: Optional[str] = None
-    ) -> str:
         ...
